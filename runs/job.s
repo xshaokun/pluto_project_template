d0aaf9cccb583f22960db054fa6a10f7575fde6a
xshaokun <xshaokun@foxmail.com>
2022-02-10 23:40:48 +0800

    only link executable script to runs

---
+++ runs/job.s
@@ -0,0 +1,7 @@
+#BSUB -J p2r003-thin-coarse-trc
+#BSUB -q astron
+#BSUB -n 100
+#BSUB -o ./p2r003-trc/p2r003-thin-coarse-trc.out
+#BSUB -e ./p2r003-trc/p2r003-thin-coarse-trc.err
+#BSUB -R span[ptile=25]
+mpirun -np 100 ./pluto -restart 12 -x3jet 
