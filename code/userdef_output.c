19f80893a095a7436af630b667439ebc8ad1905f
xshaokun <xshaokun@foxmail.com>
2023-02-28 15:38:57 +0800

    chore: pre-commit all files

--- code/userdef_output.c
+++ code/userdef_output.c
@@ -1,7 +1,7 @@
 #include "pluto.h"
 
 /* *************************************************************** */
-void ComputeUserVar (const Data *d, Grid *grid)
+void ComputeUserVar(const Data *d, Grid *grid)
 /*
  *
  *  PURPOSE
@@ -12,30 +12,24 @@ void ComputeUserVar (const Data *d, Grid *grid)
  *
  ***************************************************************** */
 {
-  #if EOS == PVTE_LAW 
-   int i, j, k, nv;  
-   double ***tmp, T, v[NVAR];
+#if EOS == PVTE_LAW
+  int i, j, k, nv;
+  double ***tmp, T, v[NVAR];
 
-   tmp = GetUserVar("tmp");
-   DOM_LOOP(k,j,i){
-     NVAR_LOOP(nv) v[nv] = d->Vc[nv][k][j][i];
-     GetPV_Temperature(v, &T);
-     tmp[k][j][i] = T;
-   }
-  #endif
+  tmp = GetUserVar("tmp");
+  DOM_LOOP(k, j, i) {
+    NVAR_LOOP(nv) v[nv] = d->Vc[nv][k][j][i];
+    GetPV_Temperature(v, &T);
+    tmp[k][j][i] = T;
+  }
+#endif
 }
 /* ************************************************************* */
-void ChangeOutputVar ()
-/* 
+void ChangeOutputVar()
+/*
  *
- * 
+ *
  *************************************************************** */
-{ 
+{
   Image *image;
-
 }
-
-
-
-
-
