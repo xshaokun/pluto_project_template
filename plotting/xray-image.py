d1a4070a3545842520561a5cb038d2042a04a5a3
Shaokun Xie <xshaokun@foxmail.com>
2023-02-28 15:57:40 +0800

    chore: setup pre commit hooks and run all files (#1)
    * chore: setup pre-commit hooks
    
    * chore: pre-commit all files
    
    * chore: modify auto commit msg

--- plotting/xray-image.py
+++ plotting/xray-image.py
@@ -1,22 +1,25 @@
 #! /bin/python3
 
 import yt
-import yt_idefix
 from yt import derived_field
-from yt.utilities.chemical_formulas import compute_mu
+
 # from yt.fields.species_fields import add
 
-from yt.utilities.physical_constants import mh, kb
-from yt.units import K
 
-
-@derived_field(name=("gas","temperature"), units="K", sampling_type="cell")
+@derived_field(name=("gas", "temperature"), units="K", sampling_type="cell")
 def _temperature(field, data):
-    return data.ds.arr(data["idefix-vtk","TMP"], "K")
+    return data.ds.arr(data["idefix-vtk", "TMP"], "K")
 
-ds = yt.load("p2r003-trc/data.0016.vtk", definitions_header="definitions.h", default_species_fields="ionized")
 
-xray_fields = yt.add_xray_emissivity_field(ds, 0.4, 5.0, metallicity=0.15, table_type="apec")
+ds = yt.load(
+    "p2r003-trc/data.0016.vtk",
+    definitions_header="definitions.h",
+    default_species_fields="ionized",
+)
+
+xray_fields = yt.add_xray_emissivity_field(
+    ds, 0.4, 5.0, metallicity=0.15, table_type="apec"
+)
 
 for field in xray_fields:
     sp = yt.ProjectionPlot(ds, "x", field, origin="native")
--- runs/job.s
+++ runs/job.s
@@ -4,4 +4,4 @@
 #BSUB -o ./p2r003-trc/p2r003-thin-coarse-trc.out
 #BSUB -e ./p2r003-trc/p2r003-thin-coarse-trc.err
 #BSUB -R span[ptile=25]
-mpirun -np 100 ./pluto -restart 12 -x3jet 
+mpirun -np 100 ./pluto -restart 12 -x3jet
